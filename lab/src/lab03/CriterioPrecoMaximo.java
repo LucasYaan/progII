537855b62b3cb4ee6da866d5e672b69baa17dd0c
Lucas Yan <164198570+LucasYaan@users.noreply.github.com>
2025-09-05 22:52:37 +0000

    chore: adjusting folder structure

--- lab/src/lab03/CriterioPrecoMaximo.java
+++
@@ -1,7 +0,0 @@
-public class CriterioPrecoMaximo implements CriterioBusca {
-    public boolean testar(Produto p, String valor) {
-        if (p.getPreco() <= Double.parseDouble(valor)) {
-            return p.getDescricao().equals(valor);
-        }
-    }
-}
